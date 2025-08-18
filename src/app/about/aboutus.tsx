b2278737ef1de71b6a5f0a238595c3baba737367
Mridul Mathur <mridulmathur2630@gmail.com>
2025-08-18 11:22:11 +0530

    updated .eslintrc.json

--- src/app/about/aboutus.tsx
+++ src/app/about/aboutus.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import TextFormatter from "@/components/text-format";
+import React, { useEffect, useState } from 'react';
+import TextFormatter from '@/components/text-format';
 
 interface AboutAboutContent {
   title: string;
@@ -7,35 +7,35 @@ interface AboutAboutContent {
   image: string;
 }
 
-function aboutus() {
+function Aboutus() {
   const [content, setContent] = useState<AboutAboutContent | null>(null);
 
   useEffect(() => {
-    fetch("/content/content.json")
+    fetch('/content/content.json')
       .then((response) => response.json())
       .then((data) => setContent(data.About.about))
-      .catch((error) => console.error("Error fetching content:", error));
+      .catch((error) => console.error('Error fetching content:', error));
   }, []);
 
   return (
-    <main className="min-h-[90vh] w-full p-4 sm:p-16">
-      <div className=" w-full ">
-        <h1 className="text-subhead pb-5 sm:pt-0 ">{content?.title}</h1>
+    <main className='min-h-[90vh] w-full p-4 sm:p-16'>
+      <div className=' w-full '>
+        <h1 className='text-subhead pb-5 sm:pt-0 '>{content?.title}</h1>
       </div>
-      <div className="flex w-full items-center gap-12 flex-row">
-        <div className="aspect-square w-2/5 border relative overflow-hidden rounded-3xl bg-slate-300">
+      <div className='flex w-full items-center gap-12 flex-row'>
+        <div className='aspect-square w-2/5 border relative overflow-hidden rounded-3xl bg-slate-300'>
           <img
             src={content?.image}
-            alt=""
-            className="w-full h-full object-cover"
+            alt=''
+            className='w-full h-full object-cover'
           />
         </div>
-        <div className="text-pretty detail w-3/5 text-para py-5 flex flex-col text-justify justify-evenly">
-          <TextFormatter text={content ? content.text : ""} />
+        <div className='text-pretty detail w-3/5 text-para py-5 flex flex-col text-justify justify-evenly'>
+          <TextFormatter text={content ? content.text : ''} />
         </div>
       </div>
     </main>
   );
 }
 
-export default aboutus;
+export default Aboutus;
